2a763cc7cd009c4b5161889b94687abce0bacc53
wlw <wlw>
2017-04-27 16:24:12 +0800

    aaa

---
+++ cir.cpp
@@ -0,0 +1,6 @@
+#include "456.h"
+
+double cirs(double s)
+{
+   return pi*s*s;
+}
---
+++ test .cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include "456.h"
+using namespace std;
+int main()
+{
+   double r,s;
+   cout<<"请输入圆的半径";
+   cin>>r;
+   s=cirs(r);
+   cout<<"圆的面积为";
+   cout<<s<<endl;
+   return 0;
+
+}
